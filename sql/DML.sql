074553890cd27e1726edf5fd7b7451ef13872391
Ethan Abraham <73045124+etabraham04@users.noreply.github.com>
2025-11-02 13:43:56 -0500

    Restructured files

---
+++ sql/DML.sql
@@ -0,0 +1,21 @@
+INSERT INTO
+	STUDENTS (FIRST_NAME, LAST_NAME, EMAIL, ENROLLMENT_DATE)
+VALUES
+	(
+		'John',
+		'Doe',
+		'john.doe@example.com',
+		'2023-09-01'
+	),
+	(
+		'Jane',
+		'Smith',
+		'jane.smith@example.com',
+		'2023-09-01'
+	),
+	(
+		'Jim',
+		'Beam',
+		'jim.beam@example.com',
+		'2023-09-02'
+	);
